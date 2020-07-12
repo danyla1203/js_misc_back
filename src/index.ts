a4b25da240579e6480fd0bb2c988d1c7a4f70f15
Daniil <zelenko.danil@gmail.com>
2020-07-12 21:21:16 +0300

    some updates

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import WebSocket from "ws";
+import WebSocket, { prototype } from "ws";
 import session from "express-session";
 import express, { Request, Response } from "express";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 import http from "http";
+import { SocketRequest, UserSocket } from "./UserSocket";
 
+const mulsterInst = multer();
 const httpApp = express();
 const sessionParser = session({
-    saveUninitialized: false,
     secret: '$eCuRiTy',
-    resave: false
+    cookie: {
+        domain: "localhost:3000",
+        path: "/"
+    }
 });
 
 httpApp.use(express.static('public'));
@@ -25,23 +30,27 @@ httpApp.get("/login", (req: Request, res: Response) => {
         res.json({ status: 401 });
     }
 })
-httpApp.post("/login",  (req: Request, res: Response) => {
+httpApp.post("/login", mulsterInst.none(), (req: Request, res: Response) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept'); 
+    console.log(req.body);
     let name = req.body.name;
     let password = req.body.password;
-    req.session.user_id = -1;
-    res.json({status: 200, name: name, password: password});
+    req.session.user_id = 1;
+    console.log(req.session);
+    let userData = { name: name, password: password }
+    res.json({status: 200, userData: userData});
 })
 
 const server = http.createServer(httpApp);
-const wss = new WebSocket.Server({ clientTracking: false, noServer: true });
+const wss = new UserSocket({ clientTracking: false, noServer: true });
 
 server.on('upgrade', function (request, socket, head) {
   console.log('Parsing session from request...');
 
   sessionParser(request, {}, () => {
     if (!request.session.user_id) {
+        console.log("her");
         socket.destroy();
         return;
     }
@@ -52,16 +61,10 @@ server.on('upgrade', function (request, socket, head) {
   });
 });
 
-wss.on("connection", (connection, request) => {
-    console.log(request.session);
-    connection.on("message", (data: string) => {
-        let parsedData = JSON.parse(data);
-        if (parsedData.action == "login") {
-            connection.send(JSON.stringify({action: parsedData.action, status: 200}));
-        } 
-    })
+wss.get("message", (request: SocketRequest) => {
+    // 1) add message to db.
+    // 2) send message to other connected users
 })
-
 
 server.listen(8080, function () {
     console.log('Listening on http://localhost:8080');
